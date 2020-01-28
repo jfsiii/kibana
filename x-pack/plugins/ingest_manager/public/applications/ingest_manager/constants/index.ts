f132430f655ca615cbb842b0047c0172df217d5c
John Schulz <john.schulz@elastic.co>
2020-01-28 14:26:01 -0500

    Fix client routes. Seems base must be /app/ID

--- x-pack/plugins/ingest_manager/public/applications/ingest_manager/constants/index.ts
+++ x-pack/plugins/ingest_manager/public/applications/ingest_manager/constants/index.ts
@@ -3,8 +3,9 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
+import { PLUGIN_ID } from '../../../../common';
 export { PLUGIN_ID, EPM_API_ROUTES, DEFAULT_AGENT_CONFIG_ID } from '../../../../common';
-export const BASE_PATH = '/app/ingest';
+export const BASE_PATH = `/app/${PLUGIN_ID}`;
 export const EPM_PATH = '/epm';
 export const AGENT_CONFIG_PATH = '/configs';
 export const FLEET_PATH = '/fleet';
